7912db6434b292ca10354812caa4063f2e7b448f
Aurélien Didier <aurelien.didier@gmail.com>
2022-01-17 21:55:10 +0100

    setup

--- setup.py
+++ setup.py
@@ -2,22 +2,7 @@
 
 from setuptools import setup, find_packages
 
-with open('README.md') as f:
-    readme_ = f.read()
-
-with open('LICENSE') as f:
-    license_ = f.read()
-
 setup(
-    name='pycaro',
-    version='0.0.1',
-    description='Validate variables',
-    long_description=readme_,
-    author='@anteverse',
-    url='https://github.com/anteverse/pycaro',
-    license=license_,
-    packages=find_packages(exclude=('tests', 'docs')),
-    python_requires=">=3.6",
-    install_requires=["click", "termcolor"],
+    packages=find_packages(exclude=("tests", "docs")),
     scripts=["bin/pycaro"],
 )
